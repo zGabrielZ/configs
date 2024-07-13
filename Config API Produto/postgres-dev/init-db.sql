c5253e9812816f3fb47ca003340dc1a2ae23998a
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2024-07-13 18:13:09 -0300

    Add database notificacao

--- Config API Produto/postgres-dev/init-db.sql
+++ Config API Produto/postgres-dev/init-db.sql
@@ -1,1 +1,14 @@
+-- Criar os bancos de dados
+CREATE DATABASE produto;
+CREATE DATABASE notificacao;
+
+-- Criar os schemas nos bancos de dados
+-- Para o banco de dados produto-dev
+\c produto;
+
+CREATE SCHEMA dev;
+
+-- Para o banco de dados notificacao-dev
+\c notificacao;
+
 CREATE SCHEMA dev;
